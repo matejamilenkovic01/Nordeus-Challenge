b9f9692be95f0bb691d6c4563346d6660c518490
Mateja Milenkovic <matejamilenkovic2001@gmail.com>
2023-11-15 14:17:43 +0100

    Dodati staticki i privatni id za AuctionId i UserId

--- src/main/java/com/nordeus/jobfair/auctionservice/auctionservice/domain/model/AuctionId.java
+++ src/main/java/com/nordeus/jobfair/auctionservice/auctionservice/domain/model/AuctionId.java
@@ -1,4 +1,16 @@
 package com.nordeus.jobfair.auctionservice.auctionservice.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public class AuctionId {
+
+    private static long nextId = 1;
+
+    private final long id;
+
+    public AuctionId() {
+        this.id = nextId++;
+    }
+
 }
--- src/main/java/com/nordeus/jobfair/auctionservice/auctionservice/domain/model/UserId.java
+++ src/main/java/com/nordeus/jobfair/auctionservice/auctionservice/domain/model/UserId.java
@@ -1,4 +1,18 @@
 package com.nordeus.jobfair.auctionservice.auctionservice.domain.model;
 
+
+
+import lombok.Getter;
+
+@Getter
 public class UserId {
+
+    private static long nextId = 1;
+
+    private final long id;
+
+    public UserId() {
+        this.id = nextId++;
+    }
+
 }
